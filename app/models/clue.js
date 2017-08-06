b58734ed47937011d0bf29289949bf60b02d06bb
Clark Jeria <clark.ajf@gmail.com>
2017-08-05 23:46:33 -0700

    removing clues ids

--- app/models/clue.js
+++ app/models/clue.js
@@ -5,6 +5,6 @@ var Schema       = mongoose.Schema;
 
 var ClueSchema   = new Schema({
     message: {type: String, required: true}
-});
+}, {_id: false});
 
 module.exports = mongoose.model('Clue', ClueSchema);
