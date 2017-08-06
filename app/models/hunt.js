12b8959b7d12b4eedcfe4667c51e1c13a224bbfb
Clark Jeria <clark.ajf@gmail.com>
2017-08-05 23:23:59 -0700

    adding embedded objects to avoid additional coding

--- app/models/hunt.js
+++ app/models/hunt.js
@@ -2,12 +2,13 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
+var Location = require('location');
 
 var HuntSchema   = new Schema({
     name: {type: String, required: true, minlength: [1, 'The string length must be between 1-15'] , maxlength: [15, 'The string length must be between 1-15']},
     shortDescription: {type: String, required: true, minlength: [1, 'The string length must be between 1-50'] , maxlength: [50, 'The string length must be between 1-50']},
     longDescription: {type: String, required: true, minlength: [1, 'The string length must be between 1-200'] , maxlength: [200, 'The string length must be between 1-200']},
-    locations: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
+    locations: [Location.schema],
     isDeleted: {type: Boolean, default: false},
     imageUrl: {type:String},
     owner: { type: Schema.Types.ObjectId, ref: 'User' },
--- app/models/location.js
+++ app/models/location.js
@@ -2,12 +2,13 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
+var Clue = require('clue');
 
 var LocationSchema   = new Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
     qrToken: {type: String, required: true},
-    clues: [{ type: Schema.Types.ObjectId, ref: 'Clue' }],
+    clues: [Clue.schema],
     imageUrl: {type: String}
 });
 
